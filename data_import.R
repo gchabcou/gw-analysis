8ab39444a5f4e62be5076502f66a9fbb646d950d
gchabcou <gchabcou@gmail.com>
2016-06-27 11:35:27 -0700

    proj file and data import

---
+++ data_import.R
@@ -0,0 +1,12 @@
+#data import - Chad VAS
+vas16<-read.csv("../Chad/2016_simple.csv",header=TRUE)
+vas15<-read.csv("../Chad/2015_simple.csv",header=TRUE) 
+vas14<-read.csv("../Chad/2014_simple.csv",header=TRUE) 
+vas13<-read.csv("../Chad/2013_simple.csv",header=TRUE) 
+vas12<-read.csv("../Chad/2012_simple.csv",header=TRUE) 
+
+human2015<-read.csv("../../Chad/human2015.csv",header=T)
+human<-read.csv("../../Chad/human_2010-2015_work.csv",header=T)
+dog2015<-read.csv("../../Chad/dog2015.csv",header=T)
+dog<-read.csv("../../Chad/dogs_2010-2015_work.csv",header=T)
+
